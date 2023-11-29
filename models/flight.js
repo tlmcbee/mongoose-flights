7d1fff0049569431f9ef63ae574a78f734b99400
tlmcbee <tlmcbeejr@gmail.com>
2023-11-29 15:52:12 -0800

    Adds destinationSchmema

--- models/flight.js
+++ models/flight.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const destinationSchema = new Schema({
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN', 'SEA']
+  },
+  arrival: Date
+})
+
 const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United', 'Alaska']},
   airport: { type: String, default: 'SEA', enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN', 'SEA']},
   flightNo: { type: Number, min: 10, max: 9999},
-  departs: { type: String, default: new Date().toLocaleString()
-    }
+  departs: { type: Date, default: new Date().toLocaleString()
+    },
+  destinations: [destinationSchema]
   } , {
   timestamps: true
 })
