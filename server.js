c3dabc211082f99cb293d042ba21c50229d8419f
tlmcbee <tlmcbeejr@gmail.com>
2023-12-01 17:39:07 -0800

    Puts in arg for method-override

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride())
+app.use(methodOverride('_method'))
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
